24dc03f7e7b81dfcb72e9773c683920db6e038f0
Matt Denwood <md@sund.ku.dk>
2021-06-24 16:03:42 +0200

    Fix bugs and introduce square landscape

--- notebooks/square_landscape.R
+++ notebooks/square_landscape.R
@@ -17,13 +17,23 @@ ggplot(landscape) + geom_sf()
 
 ## Add the hexagons:
 patches <- generate_patches(landscape, hex_width=1)
+ggplot(patches) + geom_sf()
 
-## Calculate neighbours:
 
 ## We could also make it more interesting:
 p1 <- rbind(c(0,0), c(10,0), c(30,20), c(20,40), c(10,40), c(0,0))
 p2 <- rbind(c(10,10), c(10,20), c(20,20), c(10,10))
-landscape <- st_sfc(st_cast(st_polygon(list(p1,p2)), "MULTIPOLYGON"))
+landscape <- st_sfc(st_polygon(list(p1,p2)))
 ggplot(landscape) + geom_sf()
 
-# But this fails with errors...
+patches <- generate_patches(landscape, hex_width=1)
+ggplot(patches) + geom_sf()
+
+
+## Calculate neighbours:
+neighbours <- generate_neighbours(patches)
+neighbours
+
+## So if we are currently at Index number 431 then we have 3 neighbours but only 1 with a big border:
+ggplot(patches) + aes(col=Index==431) + geom_sf()
+neighbours %>% filter(Index==431)
