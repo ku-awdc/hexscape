ca7283240eb3feff2f0cb1d385ef52ae1ecacf77
Matt Denwood <md@sund.ku.dk>
2021-07-01 21:56:01 +0200

    Add ASF dots to animations

--- presentation_2021/animations.R
+++ presentation_2021/animations.R
@@ -7,14 +7,66 @@ library("pbapply")
 library("ggpubr")
 library("sf")
 
+### Change to adjust the name of the simulated data file:
+simdatafile <- "presentation_2021/dk_model_outputs/disease_progression_per_patch_dk_ddep_20_ProcessOriented.csv"
+outfolder <- "presentation_2021/figs"
+
+## Change to zero to remove ASF dots:
+asf_size <- 0.75
+asf_alpha <- 0.5
+
 theme_set(theme_map() + theme(legend.title = element_blank()))
 # Gradient for carrying capacity colours:
 grad <- c(rgb(1,1,0.95), "blue")
 
+getmap <- function(year, iteration, simdata, patches){
+
+  if(is.numeric(iteration)){
+    simdata = simdata %>% filter(repetition_id == iteration)
+  }else{
+    stopifnot(iteration%in%c("max","mean"))
+  }
+
+  ## First get the numbers mid way through the specified year:
+  simdata <- simdata %>%
+    filter(tick <= (year*365.25)+180) %>%
+    group_by(patch_id, repetition_id) %>%
+    arrange(patch_id, repetition_id, tick) %>%
+    mutate(CumInf = cumsum(infected) > 0) %>%
+    slice(n()) %>%
+    ungroup() %>%
+    mutate(Obs = total_animals)
+
+  if(iteration %in% c("max","mean")){
+    simdata <- simdata %>%
+      group_by(patch_id) %>%
+      summarise(Mean = mean(total_animals),
+                Max = max(total_animals),
+                CumInf = mean(CumInf),
+                .groups="drop")
+
+    if(iteration=="mean"){
+      simdata <- simdata %>% select(Index = patch_id, Obs=Mean, CumInf)
+    }else if(iteration=="max"){
+      simdata <- simdata %>% select(Index = patch_id, Obs=Max, CumInf)
+    }else{
+      stop()
+    }
+
+  }else{
+    simdata <- simdata %>%
+      select(Index = patch_id, Obs, CumInf)
+  }
+
+  return(
+    simdata %>% mutate(Year = year) %>% left_join(patches, by="Index")
+  )
+}
+
 
 (load("patches/patches_denmark.rda"))
 
-simulation <- read_csv2("presentation_2021/dk_model_outputs/disease_progression_per_patch_dk_ddep_20_ProcessOriented.csv")
+simulation <- read_csv2(simdatafile)
 
 # Extent of the "focussed" map in rows/columns of hexagons:
 constrain <- function(x) x %>% filter(row < 50, col < 63, col < 54 | row < 35)
@@ -44,18 +96,22 @@ cpt <- ggplot() +
 cpt
 
 ### Create daily jpg:
-unlink("presentation_2021/figs", recursive=TRUE)
-dir.create("presentation_2021/figs")
+unlink(outfolder, recursive=TRUE)
+dir.create(outfolder)
 pblapply(plotdata, function(x){
   allyears <- gsub(" ", "0", format(1:maxyears))
+  asfpts <- x %>% constrain() %>% filter(CumInf > 0)
   pt <- ggplot(x %>% constrain()) +
     aes(fill=Obs, col=Obs, geometry=geometry) +
     geom_sf(lwd=0) +
+    ## Comment out this line to remove ASF dots:
+    geom_sf(data=asfpts, aes(geometry=centroid), col="red", fill="red", pch=20, size=asf_size, alpha=asf_alpha) +
     ggtitle(str_c("Animals after ", x$Year[1], ifelse(x$Year[1]==1, " year", " years"))) +
     scale_colour_gradient(limits = c(0, maxobs), low = grad[1], high = grad[2]) +
     scale_fill_gradient(limits = c(0, maxobs), low = grad[1], high = grad[2])
+  pt
   ggpubr::ggarrange(pt, cpt, ncol=2)
-  ggsave(str_c("presentation_2021/figs/plot", allyears[x$Year[1]], ".jpg"), width=8, height=4)
+  ggsave(str_c(outfolder, "/plot", allyears[x$Year[1]], ".jpg"), width=8, height=4)
   invisible(TRUE)
 })
 
