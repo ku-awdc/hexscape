c8cd7e812964fe4a0b6f6b975a9a5b5ffe8f2da4
Matt Denwood <md@sund.ku.dk>
2023-08-10 13:52:34 +0200

    Fix bug requiring Index for randomise voronoi

--- R/randomise_voronoi.R
+++ R/randomise_voronoi.R
@@ -6,12 +6,29 @@
 #' @param buffer
 #' @param verbose
 #'
+#' @examples
+#' xrange <- c(0, 10)
+#' yrange <- c(0, 10)
+#' corners <- tribble(~x, ~y,
+#'                    xrange[1], yrange[1],
+#'                    xrange[2], yrange[1],
+#'                    xrange[2], yrange[2],
+#'                    xrange[1], yrange[2],
+#'                    xrange[1], yrange[1]
+#' )
+#' library("sf")
+#' landscape <- st_sfc(st_multipolygon(list(list(as.matrix(corners))))) |> st_as_sf()
+#' points <- st_sfc(lapply(1:10, function(x) st_point(runif(2,0,10)))) |> st_as_sf()
+#' random <- randomise_voronoi(landscape, points) |> mutate(Index = 1:n())
+#' ggplot(random) + geom_sf_label(aes(label=Index)) + geom_sf_label(aes(geometry=RandomPoint, label=Index), col="red")
+#'
+#'
 #' @export
 randomise_voronoi <- function(map, points, randomise_size=5L, sample_size=10L, max_tries=3L, verbose=1L){
 
   ## TODO: add buffer to stop two points being close to each other (as an argument to sample_points)
   ## TODO: change active geometry to RandomPoint in return value (and maybe add the line showing the change, as well as a jitter distance etc?)
-  
+
 
   stopifnot(inherits(map, "sf"), inherits(map, "data.frame"))
   stopifnot(inherits(points, "sf"), inherits(points, "data.frame"))
@@ -20,7 +37,7 @@ randomise_voronoi <- function(map, points, randomise_size=5L, sample_size=10L, m
   mapsf <- st_union(map)
 
   ## Delegate to get Voronoi tesselation:
-  voronoi <- discretise_voronoi(map, points)
+  voronoi <- discretise_voronoi(map, points) |> mutate(Index = 1:n())
 
   ## Delegate to get sampled points:
   cat("Getting random points...\n")
@@ -29,7 +46,7 @@ randomise_voronoi <- function(map, points, randomise_size=5L, sample_size=10L, m
 
   ## Then use pairwise distance matrices to get the closest S centroids to
   ## each point:
-  st_distance(voronoi$point, voronoi$centroid) |>
+  st_distance(voronoi, voronoi$centroid) |>
     apply(2, function(x) order(x)[1:randomise_size], simplify=FALSE) ->
     closest
   stopifnot(nrow(voronoi)==length(closest))
