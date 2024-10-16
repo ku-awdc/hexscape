7746e87deaa70d3e491d7b0b55361cbc959b9e51
CGMossa <cgmossa@gmail.com>
2024-10-16 23:13:05 +0200

    Delete notebooks/test_autoplot_load_map.R

--- notebooks/test_autoplot_load_map.R
+++
@@ -1,8 +0,0 @@
-
-devtools::document()
-devtools::load_all()
-
-# download_maps()
-load_map("DK031") |> autoplot()
-load_map("DK031") |> autoplot() + aes(fill = Label)
-load_map("DK031") |> autoplot() + scale_fill_manual()
