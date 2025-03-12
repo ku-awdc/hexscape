69d0e5663c2b931d0afce3efa8c55fcc58f9a35b
Matt Denwood <md@sund.ku.dk>
2025-03-12 18:04:18 +0100

    Add S7

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,9 @@
 .onLoad <- function(lib, pkg)
 {
+  .onLoad <- function(...) {
+    S7::methods_register()
+  }
+
   ## Set the storage folder if the global variable is found:
   sf <- Sys.getenv("HEXSCAPE_STORAGE")
   if(sf!="" && dir.exists(sf)){
